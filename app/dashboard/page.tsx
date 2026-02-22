ba8b8776d2042940d6eb46083db5cc3b7b736e93
IvanLuketic24 <luketici91@gmail.com>
2026-02-22 01:07:36 +0100

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,1 @@
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
-import DashboardClient from "./DashboardClient";
-
-export default async function DashboardPage() {
-  const { userId } = await auth();
-  if (!userId) redirect("/sign-in");
-
-  return <DashboardClient />;
-}
+redirect("/sign-in");
